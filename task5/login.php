305db79e9198e61ea8c2e3d4aea780fb4fc4f8e8
Ksenyakin Daniil <123-510@mail.ru>
2023-05-09 22:56:39 +0300

    task5

--- task5/login.php
+++ task5/login.php
@@ -1,54 +1,66 @@
 <?php
-
-/**
- * Файл login.php для не авторизованного пользователя выводит форму логина.
- * При отправке формы проверяет логин/пароль и создает сессию,
- * записывает в нее логин и id пользователя.
- * После авторизации пользователь перенаправляется на главную страницу
- * для изменения ранее введенных данных.
- **/
-
-// Отправляем браузеру правильную кодировку,
-// файл login.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
-
-// Начинаем сессию.
 session_start();
 
-// В суперглобальном массиве $_SESSION хранятся переменные сессии.
-// Будем сохранять туда логин после успешной авторизации.
 if (!empty($_SESSION['login'])) {
-  // Если есть логин в сессии, то пользователь уже авторизован.
-  // TODO: Сделать выход (окончание сессии вызовом session_destroy()
-  //при нажатии на кнопку Выход).
-  // Делаем перенаправление на форму.
   header('Location: ./');
 }
 
-// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
-// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-?>
+  $messages = array();
+  $errors = array();
+  $errors['login1'] = !empty($_COOKIE['login_error1']);
+  $errors['login2'] = !empty($_COOKIE['login_error2']);
+  $errors['password'] = !empty($_COOKIE['password_error']);
 
-<form action="" method="post">
-  <input name="login" />
-  <input name="pass" />
-  <input type="submit" value="Войти" />
-</form>
+  if (!empty($errors['login1'])) {
+    setcookie('login_error1', '', 100000);
+    $messages['login1'] = '<p class="msg">Вы не заполнили логин</p>';
+  }
+  if (!empty($errors['login2'])) {
+    setcookie('login_error2', '', 100000);
+    $messages['login2'] = '<p class="msg">Такого аккаунта не существует</p>';
+  }
+  if (!empty($errors['password'])) {
+    setcookie('password_error', '', 100000);
+    $messages['password'] = '<p class="msg">Вы не заполнили пароль</p>';
+  }
 
-<?php
+  include('loginform.php');
 }
-// Иначе, если запрос был методом POST, т.е. нужно сделать авторизацию с записью логина в сессию.
 else {
+  $login = $_POST['login'];
+  $password = $_POST['password'];
 
-  // TODO: Проверть есть ли такой логин и пароль в базе данных.
-  // Выдать сообщение об ошибках.
+  if (empty($login)) {
+    setcookie('login_error1', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  if (empty($password)) {
+    setcookie('password_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
 
-  // Если все ок, то авторизуем пользователя.
-  $_SESSION['login'] = $_POST['login'];
-  // Записываем ID пользователя.
-  $_SESSION['uid'] = 123;
+  if ($errors) {
+    header('Location: login.php');
+    exit();
+  }
 
-  // Делаем перенаправление.
-  header('Location: ./');
+  $user = 'u52852';
+  $pass = '3064314';
+  $db = new PDO('mysql:host=localhost;dbname=u52852', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+  $stmt = $db->prepare('SELECT user_id FROM users WHERE (login = ?) AND (password = ?) ');
+  $stmt->execute([$login, md5($password)]);
+
+  if ($stmt->rowCount() > 0) {
+    $_SESSION['login'] = $_POST['login'];
+    $stmt = $db->prepare("SELECT application_id FROM users WHERE login = ?");
+    $stmt->execute([$login]);
+    $_SESSION['uid'] = $stmt->fetchColumn();
+    header('Location: ./');
+  } else {
+    setcookie('login_error2', '1', time() + 24 * 60 * 60);
+    header('Location: login.php');
+    exit();
+  }
 }
